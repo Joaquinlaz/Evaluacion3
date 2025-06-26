68595a9d1b8083fe9fa95725f8808dcebdb0897d
000francisca0 <fr.sanhuezap@duocuc.cl>
2025-06-25 20:12:20 -0400

    Delete pedidos directory

--- pedidos/src/main/java/com/ecomarket/ecomarket/Repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecomarket.ecomarket.Repository;
-
-import com.ecomarket.ecomarket.Model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
